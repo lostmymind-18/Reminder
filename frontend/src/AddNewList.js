aa0b4f994667ca4552656a8ca59093aacba83376
lostmymind-18 <supercool1170@gmail.com>
2024-04-13 20:36:09 +0700

    need to learn more about css layout

--- frontend/src/AddNewList.js
+++ frontend/src/AddNewList.js
@@ -2,15 +2,17 @@ import './AddNewList.css'
 
 export default function AddNewList({onAddNewListChange}){
     return <div className='AddNewList'>
-        <h3>New list</h3>
+        <h3 style={{color:'white'}}>New list</h3>
         <div className="input">
-            <input/>
+           <input/> <span>Name:</span> 
         </div>
-        <div className="color-selection">
-        
-        </div>
-        <div className="icon-selection">
-
+        <div className="row">
+            <div className="color-selection">
+                Color: 
+            </div>
+            <div className="icon-selection">
+                Icon: 
+            </div>
         </div>
         <button onClick={()=>{onAddNewListChange(false)}}>Close</button>
     </div>
